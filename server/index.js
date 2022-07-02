dea7681b3e58f9e435ad3a9812173836542abba3
Brennan Skinner <bgskinner3@gmail.com>
2022-07-02 01:17:19 -0400

    cors route

--- server/index.js
+++ server/index.js
@@ -46,9 +46,9 @@ const startServer = async () => {
   app.use('*', cors());
 
   app.use(express.static(path.join(__dirname, '../build')));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'));
-    //  //'../build/index.html'
+   
   });
   app.use(errorHandler);
   server.applyMiddleware({ path: '/graphql', app });
